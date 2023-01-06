44e35eadec5e58b84bda024ed5cf871f2a11277e
Blob <alteringorbisgames@gmail.com>
2023-01-06 10:49:49 -0400

    * 2.17.28.66 | Changed Configuration To Release For Github Release

--- Common.cs
+++ Common.cs
@@ -75,7 +75,8 @@ namespace Dobby {
             "* 2.13.26.55 | Swapped Out FormMove Func On More Forms, Credits Page Spacing And Text Tweak, Moved The CreditsBtn Function For The Main Page And Tweaked Button Positioning, Other",
             "* 2.14.27.57 | Resized All Forms To Simplify Message Box Creation, Message Box Changes (Almost Done...), Other Things I Forget But Know I Did Because I Updated The Build # After Doing 'Em...",
             "* 2.15.27.57 | Moved The Page Title To The Top Of The Page Now That I Can Use Controls As Drag Anchors Instead Of Only The Form",
-            "* 2.17.28.66 | UC4 1.3X MP Restored Debug Finally Finished, Only Need To Add Alternative With The Skipped Stuff, Plus Illusions's Stuff | Added Function Pointers For Form Move Functions To CSTMDebugPage | Repositioned The Page Titles"
+            "* 2.17.28.66 | UC4 1.3X MP Restored Debug Finally Finished, Only Need To Add Alternative With The Skipped Stuff, Plus Illusions's Stuff | Added Function Pointers For Form Move Functions To CSTMDebugPage | Repositioned The Page Titles",
+            "* 2.17.28.66 | Changed Configuration To Release For Github Release"
 
             // TODO: EbootPatchHelpPage
 
@@ -210,7 +211,7 @@ namespace Dobby {
 
         public class Dev {
 
-            public const bool REL = false
+            public const bool REL = true
                                          ;
 
             public static Thread DebuggerThread = new Thread(new ThreadStart(UpdateConsoleOutput));
