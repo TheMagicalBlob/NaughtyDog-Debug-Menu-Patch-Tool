28195b9ac014508a1ed16fbed6455e2e1f6972f8
TheMagicalBlob <alteringorbisgames@gmail.com>
2024-02-04 02:09:10 -0400

    3.42.185.497 | Small BootSettings Fix

--- Common.cs
+++ Common.cs
@@ -213,6 +213,8 @@ namespace Dobby {
           "* 3.40.182.491 | PS4MenuSettingsPatchPage Work, Removed Old Debug Output Calls",
           "* 3.42.183.494 | PS4MenuSettingsPatchPage Now Functions, Pointers Still Need To Be Added For The Majority Of Cases, Debug Output Changes",
           "* 3.42.184.496 | Replaced BootSettings data with updated code, Debug Output Tweak",
+          "* 3.42.185.497 | Small BootSettings Fix"
+
 
             // TODO:
             // * MAJOR
